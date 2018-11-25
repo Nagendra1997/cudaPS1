fe3b0a21b66452118ccc0cdef85907f5998d92bb
Nagendra1997 <nagendray.ec14@rvce.edu.in>
2018-11-25 21:12:04 +0530

    .

--- Problem Set 1/HW1.cpp
+++ Problem Set 1/HW1.cpp
@@ -77,7 +77,7 @@ void cleanup()
 
 void generateReferenceImage(std::string input_filename, std::string output_filename)
 {
-  cv::Mat reference = cv:imread(input_filename, 0);
+  cv::Mat reference = cv:imread(input_filename, CV_LOAD_IMAGE_GRAYSCALE );
 
   cv::imwrite(output_filename, reference);
 
