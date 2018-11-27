93c14b5d7cbfd15538a0f86c7849112b163c0d88
Nagendra1997 <nagendray.ec14@rvce.edu.in>
2018-11-27 15:54:25 +0530

    img files

--- Problem Set 1/HW1.cpp
+++ Problem Set 1/HW1.cpp
@@ -56,13 +56,13 @@ void preProcess(uchar4 **inputImage, unsigned char **greyImage,
   checkCudaErrors(cudaMalloc(d_rgbaImage, sizeof(uchar4) * numPixels));
   checkCudaErrors(cudaMalloc(d_greyImage, sizeof(unsigned char) * numPixels));
   checkCudaErrors(cudaMemset(*d_greyImage, 0, numPixels * sizeof(unsigned char))); //make sure no memory is left laying around
-  cudaDeviceSynchronize();
+  cudaDeviceSynchronize();checkCudaErrors(cudaGetLastError());
   
 
   
   //copy input array to the GPU
   checkCudaErrors(cudaMemcpy(*d_rgbaImage, *inputImage, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
- cudaDeviceSynchronize();
+ cudaDeviceSynchronize();checkCudaErrors(cudaGetLastError());
  std::cout << "scukkkkkkkkkk it" << std::endl;
   d_rgbaImage__ = *d_rgbaImage;
   d_greyImage__ = *d_greyImage;
